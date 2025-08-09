01ab9a6ff78c081fd854f5709fa582b99ce8bb30
qazwsxedcrfv-jap <75927973+qazwsxedcrfv-jap@users.noreply.github.com>
2025-08-09 14:04:47 +0900

    service worker

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/animal-feeding-electron-app/sw.js', { scope: '/animal-feeding-electron-app/' })})}
--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
   server: {
     port: 5173,
   },
-  base: './',
+  base: '/animal-feeding-electron-app/',
   build: {
     outDir: 'docs',
   },
